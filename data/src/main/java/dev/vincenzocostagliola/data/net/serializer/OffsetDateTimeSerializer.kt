3e8d9b0e01472bffda0ea079e6c47469a81b77e8
Vincenzo Costagliola <vincenzocostagliola@gmail.com>
2025-04-20 16:31:53 +0200

    add toDomain functions for getCoindata

---
+++ data/src/main/java/dev/vincenzocostagliola/data/net/serializer/OffsetDateTimeSerializer.kt
@@ -0,0 +1,29 @@
+package dev.vincenzocostagliola.data.net.serializer
+
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.format.DateTimeFormatter
+
+
+class OffsetDateTimeSerializer : KSerializer<OffsetDateTime> {
+
+    private val format = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("OffsetDateTime", PrimitiveKind.STRING)
+
+    override fun serialize(encoder: Encoder, value: OffsetDateTime) {
+        val string = format.format(value)
+        encoder.encodeString(string)
+    }
+
+    override fun deserialize(decoder: Decoder): OffsetDateTime {
+        val string = decoder.decodeString()
+        return format.parse(string, OffsetDateTime.FROM)
+    }
+}
