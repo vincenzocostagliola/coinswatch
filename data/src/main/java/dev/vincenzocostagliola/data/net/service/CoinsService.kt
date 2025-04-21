d1598fada788a13dfed3f216e874911135cb635a
Vincenzo Costagliola <vincenzocostagliola@gmail.com>
2025-04-21 21:33:24 +0200

    fixed header

--- data/src/main/java/dev/vincenzocostagliola/data/net/service/CoinsService.kt
+++ data/src/main/java/dev/vincenzocostagliola/data/net/service/CoinsService.kt
@@ -4,10 +4,7 @@ import com.skydoves.sandwich.ApiResponse
 import dev.vincenzocostagliola.data.dto.CoinDataDto
 import dev.vincenzocostagliola.data.dto.CoinDto
 import dev.vincenzocostagliola.data.dto.CoinHistoricalDataDto
-import dev.vincenzocostagliola.data.net.NetHelper
 import retrofit2.http.GET
-import retrofit2.http.Headers
-import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface CoinsService {
@@ -16,16 +13,14 @@ interface CoinsService {
     suspend fun getCoinsWithMarketData(@Query("vs_currency") currency: String): ApiResponse<List<CoinDto>>
 
     /*---COIN data---*/
-    @Headers(NetHelper.APIKEY)
     @GET("coins/id")
-    suspend fun getCoinData(@Path("id") coinId: String): ApiResponse<CoinDataDto>
+    suspend fun getCoinData(@Query("id") coinId: String): ApiResponse<CoinDataDto>
 
     /*---COIN Historical data---*/
-    @Headers(NetHelper.APIKEY)
     @GET("coins/{id}/market_char?precision=2")
     suspend fun getCoinHistoricalData(
-        @Path("id") coinId: String,
-        @Path("vs_currency") currency: String,
-        @Path("days") days: Int
+        @Query("id") coinId: String,
+        @Query("vs_currency") currency: String,
+        @Query("days") days: Int
     ): ApiResponse<List<CoinHistoricalDataDto>>
 }
