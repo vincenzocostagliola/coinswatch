c843d26cc42f9f518667d3407f7090b748389954
Vincenzo Costagliola <vincenzocostagliola@gmail.com>
2025-04-21 09:19:10 +0200

    fixed card

--- app/src/main/java/dev/vincenzocostagliola/coinswatch/ui/OutlinedCard.kt
+++ app/src/main/java/dev/vincenzocostagliola/coinswatch/ui/OutlinedCard.kt
@@ -30,12 +30,11 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
+import dev.vincenzocostagliola.coinswatch.R
 import dev.vincenzocostagliola.data.domain.Coin
-import org.threeten.bp.LocalDate
+
 
 @Composable
 fun OutlinedCard(item: Coin) {
@@ -48,16 +47,16 @@ fun OutlinedCard(item: Coin) {
             .padding(top = Dimens.XRegular)
     ) {
         AsyncImage(
-            model = item.shortInfo.coverImageUrl,
+            model = item.image,
             modifier = Modifier
                 .clip(CardDefaults.outlinedShape)
                 .aspectRatio(16 / 9f),
             contentScale = ContentScale.Crop,
-            contentDescription = item.shortInfo.title
+            contentDescription = item.name
         )
         Column(Modifier.padding(start = Dimens.XRegular, end = Dimens.XRegular)) {
             Row(verticalAlignment = Alignment.CenterVertically) {
-                Text(item.shortInfo.title, style = MaterialTheme.typography.titleLarge)
+                Text(item.name, style = MaterialTheme.typography.titleLarge)
                 Spacer(Modifier.weight(1f))
                 IconButton(onClick = { expanded = expanded.not() }) {
                     Icon(
@@ -70,21 +69,21 @@ fun OutlinedCard(item: Coin) {
             Spacer(Modifier.height(Dimens.XSmall))
             CreateShortInfoBox(
                 expanded = expanded,
-                shortInfo = item.shortInfo
+                coin = item
             )
         }
     }
 }
 
 @Composable
-private fun ColumnScope.CreateShortInfoBox(expanded: Boolean, shortInfo: ShortInfo) {
+private fun ColumnScope.CreateShortInfoBox(expanded: Boolean, coin: Coin) {
     Box {
         this@CreateShortInfoBox.AnimatedVisibility(visible = expanded) {
             Row(
                 Modifier.fillMaxWidth()
             ) {
-                StartColumn(shortInfo, Modifier.weight(weight = 1F))
-                EndColumn(shortInfo, Modifier.weight(weight = 1F))
+                StartColumn(coin, Modifier.weight(weight = 1F))
+                EndColumn(coin, Modifier.weight(weight = 1F))
             }
         }
     }
@@ -94,34 +93,25 @@ private fun ColumnScope.CreateShortInfoBox(expanded: Boolean, shortInfo: ShortIn
 }
 
 @Composable
-private fun StartColumn(shortInfo: ShortInfo, modifier: Modifier) {
+private fun StartColumn(coin: Coin, modifier: Modifier) {
     Column(
         verticalArrangement = Arrangement.spacedBy(Dimens.XSmall),
         modifier = modifier.fillMaxSize()
     ) {
+
         Text(
-            text = stringResource(R.string.album_number),
+            text = coin.id,
             style = MaterialTheme.typography.bodyLarge
         )
         Text(
-            text = shortInfo.number?.let {
-                it.toString()
-            } ?: stringResource(R.string.no_value),
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Text(
-            text = stringResource(R.string.periodicity),
-            style = MaterialTheme.typography.bodyLarge
-        )
-        Text(
-            text = shortInfo.periodicity.name,
+            text = coin.marketCapRank.toString(),
             style = MaterialTheme.typography.bodyLarge
         )
     }
 }
 
 @Composable
-private fun EndColumn(shortInfo: ShortInfo, modifier: Modifier) {
+private fun EndColumn(coin: Coin, modifier: Modifier) {
     Column(
         verticalArrangement = Arrangement.spacedBy(Dimens.XSmall),
         modifier = modifier.fillMaxSize()
@@ -144,41 +134,11 @@ private fun EndColumn(shortInfo: ShortInfo, modifier: Modifier) {
         )
     }
 }
-
+/*
 
 @Preview
 @Composable
 fun CardPreview() {
-    OutlinedCard(getFakeAlbum())
+    OutlinedCard()
 }
-
-fun getFakeAlbum(): CollectionItem.DylanDog {
-    return CollectionItem.DylanDog(
-        shortInfo = ShortInfo(
-            number = 10,
-            coverImageUrl = "https://shop.sergiobonelli.it/resizer/1000/-1/true/1601882082011.png--dylan_dog_oldboy_3_cover.png?1601882082000",
-            title = "Dylan Dog Oldboy 3",
-            periodicity = Details.Periodicity.BIMESTRALE,
-            summary = "cose da raccontare"
-        ), details = Details(
-            team = Details.Team(
-                subject = listOf("Simeoni Gigi", "Marzano Giancarlo"),
-                script = listOf("Simeoni Gigi", "Marzano Giancarlo"),
-                illustrations = listOf(
-                    "Simeoni Gigi",
-                    "Camagni Giulio"
-                ),
-                cover = listOf(
-                    "Cestaro Raul",
-                    "Cestaro Gianluca"
-                )
-            ), album = Details.Album(
-                format = "16x21 cm, b/n",
-                pages = 192,
-                isbn = "977182645204500100",
-                dateOfPublishing = LocalDate.now(),
-                albumUrl = "https://shop.sergiobonelli.it/dylan-dog/2020/08/26/albo/dylan-dog-oldboy-3-1008607/"
-            )
-        ), id = 9990
-    )
-}
+*/
