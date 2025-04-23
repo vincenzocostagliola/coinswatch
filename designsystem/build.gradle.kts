d727c2e7b2159cbf5aeaba444ccbd0566f85238e
vince <vincenzo.costagliola@develhope.co>
2025-04-23 21:03:33 +0200

    added design system module

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -1,9 +1,3 @@
-@file:Suppress("DSL_SCOPE_VIOLATION")
-
-import com.android.build.api.dsl.Packaging
-
-//Because of https://github.com/gradle/gradle/issues/22797
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -13,28 +7,10 @@ plugins {
 }
 
 android {
-    namespace = "dev.vincenzocostagliola.coinswatch"
+    namespace = "dev.vincenzocostagliola.designsystem"
+    testNamespace = "dev.vincenzocostagliola"
     compileSdk = 35
 
-    defaultConfig {
-        applicationId = "dev.vincenzocostagliola.coinswatch"
-        minSdk = 26
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
@@ -42,20 +18,6 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_21.toString()
     }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.7"
-    }
-    buildToolsVersion = "35.0.0"
-
-    fun Packaging.() {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-
 }
 
 dependencies {
@@ -77,8 +39,4 @@ dependencies {
     ksp(libs.hilt.compiler)
     implementation(libs.timber)
     implementation(libs.threeTen)
-
-    /**MODULES*/
-    implementation(project(":data"))
-    implementation(project(":designsystem"))
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,3 +22,4 @@ dependencyResolutionManagement {
 rootProject.name = "coinswatch"
 include(":app")
 include(":data")
+include(":designsystem")
