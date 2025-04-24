aa54678a9f3d18eab03c3d73af8a13e1ba099b15
vince <vincenzo.costagliola@develhope.co>
2025-04-24 08:41:53 +0200

    added states and events to DetailScreenViewModel

--- app/src/main/java/dev/vincenzocostagliola/coinswatch/DetailScreenViewModel.kt
+++ app/src/main/java/dev/vincenzocostagliola/coinswatch/DetailScreenViewModel.kt
@@ -1,12 +1,41 @@
 package dev.vincenzocostagliola.coinswatch
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.vincenzocostagliola.data.domain.Coin
+import dev.vincenzocostagliola.data.error.CoinSwatchError
 import dev.vincenzocostagliola.data.net.repository.Repository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+sealed class DetailScreenState {
+    data object Loading : DetailScreenState()
+    data class Success(val list: List<Coin>) : DetailScreenState()
+    data class Error(val error: CoinSwatchError) : DetailScreenState()
+}
+
+sealed class DetailScreenEvents {
+    data class GetCoinData(val coinId: String) : DetailScreenEvents()
+}
+
 @HiltViewModel
-class DetailScreenViewModel  @Inject constructor(
+class DetailScreenViewModel @Inject constructor(
     private val repository: Repository
 ) : ViewModel() {
+
+    private val _detailScreenState: MutableStateFlow<DetailScreenState> =
+        MutableStateFlow(DetailScreenState.Loading)
+    val detailScreenState: StateFlow<DetailScreenState>
+        get() = _detailScreenState
+
+    fun sendEvent(event: DetailScreenEvents) {
+        viewModelScope.launch() {
+            when (event) {
+                is DetailScreenEvents.GetCoinData -> Unit
+            }
+        }
+    }
 }
