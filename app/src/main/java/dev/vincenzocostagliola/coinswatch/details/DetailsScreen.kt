942e24bede1cf0fea9d3abf26c990e862f8513ff
vince <vincenzo.costagliola@develhope.co>
2025-04-27 00:12:12 +0200

    removed lazycolumn in details screen

--- app/src/main/java/dev/vincenzocostagliola/coinswatch/details/DetailsScreen.kt
+++ app/src/main/java/dev/vincenzocostagliola/coinswatch/details/DetailsScreen.kt
@@ -1,6 +1,7 @@
 package dev.vincenzocostagliola.coinswatch.details
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,6 +10,8 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
@@ -98,22 +101,19 @@ private fun ShowDetails(
             )
         }
     ) {
-        LazyColumn(
+        Column(
             modifier = modifier
                 .padding(it)
                 .fillMaxSize()
                 .padding(Dimens.XRegular)
+                .verticalScroll(rememberScrollState())
         ) {
-            items(1) {
-                ShowImage(data.image.large, data.name)
-                data.url?.let {
-                    ShowHomeLink(it)
-                }
-                ShowDescription(data.description, goToDescription)
-                ShowChart(data.history)
+            ShowImage(data.image.large, data.name)
+            data.url?.let {
+                ShowHomeLink(it)
             }
-            //ShowHistory(data.history)
-
+            ShowDescription(data.description, goToDescription)
+            ShowChart(data.history)
         }
     }
 }
