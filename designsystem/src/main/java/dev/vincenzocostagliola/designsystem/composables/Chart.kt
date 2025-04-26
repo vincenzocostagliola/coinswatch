eab567a1029b8ba2619015cbbbf7351502e0f5f4
vince <vincenzo.costagliola@develhope.co>
2025-04-26 23:13:46 +0200

    updated chart with dates

--- designsystem/src/main/java/dev/vincenzocostagliola/designsystem/composables/Chart.kt
+++ designsystem/src/main/java/dev/vincenzocostagliola/designsystem/composables/Chart.kt
@@ -1,54 +1,108 @@
 package dev.vincenzocostagliola.designsystem.composables
 
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import dev.vincenzocostagliola.designsystem.theme.Pink80
 import dev.vincenzocostagliola.designsystem.theme.Purple80
+import dev.vincenzocostagliola.designsystem.values.Dimens.Regular
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.ZoneOffset
+import org.threeten.bp.format.DateTimeFormatter
+import org.threeten.bp.format.DateTimeFormatter.ISO_DATE
+import org.threeten.bp.format.FormatStyle
 import timber.log.Timber
+import java.time.format.FormatStyle.SHORT
 
 @Composable
-@Preview(heightDp = 300, widthDp = 300, backgroundColor = 0xFFFFFFFF, showBackground = true)
-fun Chart(modifier: Modifier = Modifier, list: List<Float> = listOf(1f, 20f, 3f, 1f)) {
+fun Chart(modifier: Modifier = Modifier, list: List<Float>, dates: List<OffsetDateTime>) {
     val zipList: List<Pair<Float, Float>> = list.zipWithNext()
     Timber.d("ChartComposable - prices: $zipList")
 
+    Column(modifier = modifier.fillMaxWidth().padding(top = Regular)) {
+        Row(modifier = modifier.height(250.dp)) {
+            val max = list.max()
+            val min = list.min()
 
-    Row(modifier = modifier) {
-        val max = list.max()
-        val min = list.min()
+            val lineColor =
+                if (list.last() > list.first()) Purple80 else Pink80
 
-        val lineColor =
-            if (list.last() > list.first()) Purple80 else Pink80
+            for (pair in zipList) {
 
-        for (pair in zipList) {
+                val fromValuePercentage = getValuePercentageForRange(pair.first, max, min)
+                val toValuePercentage = getValuePercentageForRange(pair.second, max, min)
 
-            val fromValuePercentage = getValuePercentageForRange(pair.first, max, min)
-            val toValuePercentage = getValuePercentageForRange(pair.second, max, min)
+                Canvas(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .weight(1f),
+                    onDraw = {
+                        val fromPoint =
+                            Offset(x = 0f, y = size.height.times(1 - fromValuePercentage))
+                        val toPoint =
+                            Offset(x = size.width, y = size.height.times(1 - toValuePercentage))
 
-            Canvas(
-                modifier = Modifier
-                    .fillMaxHeight()
-                    .weight(1f),
-                onDraw = {
-                    val fromPoint = Offset(x = 0f, y = size.height.times(1 - fromValuePercentage))
-                    val toPoint =
-                        Offset(x = size.width, y = size.height.times(1 - toValuePercentage))
-
-                    drawLine(
-                        color = lineColor,
-                        start = fromPoint,
-                        end = toPoint,
-                        strokeWidth = 3f
-                    )
-                })
+                        drawLine(
+                            color = lineColor,
+                            start = fromPoint,
+                            end = toPoint,
+                            strokeWidth = 3f
+                        )
+                    })
+            }
+        }
+        Row(
+            modifier = modifier.fillMaxWidth().padding(top = Regular),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            dates.forEach {
+                Text(
+                    text = it.format(dateFormatter),
+                    style = extraSmallText
+                )
+            }
         }
     }
 }
 
+private val dateFormatter : DateTimeFormatter = DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
+
 private fun getValuePercentageForRange(value: Float, max: Float, min: Float) =
     (value - min) / (max - min)
+
+
+@Preview(heightDp = 300, widthDp = 300, backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+private fun ShowChart() {
+    val now = OffsetDateTime.of(2025, 5, 1, 1, 0, 0, 0, ZoneOffset.UTC)
+    val datesList = listOf<OffsetDateTime>(
+        now,
+        now.plusDays(1),
+        now.plusDays(2),
+        now.plusDays(3),
+        now.plusDays(4),
+        now.plusDays(5),
+        now.plusDays(6),
+        now.plusDays(7)
+    )
+
+    val values = listOf(40f, 20f, 3f, 1f, 5f, 10f, 5f)
+
+    Chart(
+        modifier = Modifier,
+        list = values,
+        dates = datesList
+    )
+}
