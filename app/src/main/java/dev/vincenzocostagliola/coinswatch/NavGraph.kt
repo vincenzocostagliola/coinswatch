6d6240bf1664ffe5eea11923d67a1bf560448ad5
vince <vincenzo.costagliola@develhope.co>
2025-04-27 11:12:56 +0200

    clean up

--- app/src/main/java/dev/vincenzocostagliola/coinswatch/NavGraph.kt
+++ app/src/main/java/dev/vincenzocostagliola/coinswatch/NavGraph.kt
@@ -20,20 +20,20 @@ sealed class NavigationRoute() {
     abstract val route: String
 
     data object Home : NavigationRoute() {
-        override val route: String = "home"
+        override val route: String = "Home"
     }
 
     data object DetailsScreen : NavigationRoute() {
         fun createRoute(coinId: String) = "$screenName?$argumentId=${coinId}"
         const val argumentId: String = "coinId"
-        private val screenName: String = "detailScreen"
+        private val screenName: String = "DetailScreen"
         override val route: String = "$screenName?$argumentId={$argumentId}"
     }
 
     data object DescriptionScreen : NavigationRoute() {
         fun createRoute(description: String) = "$screenName?$argumentId=${description}"
         const val argumentId: String = "description"
-        private val screenName: String = "DescriptionScreen"
+        private const val screenName: String = "DescriptionScreen"
         override val route: String = "$screenName?$argumentId={$argumentId}"
     }
 }
